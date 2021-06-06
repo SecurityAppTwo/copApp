e06c31eae7383245c6e8c9b4f6d4bea11c4d54e0
hackathon IDF <hack@cs.colman.ac.il>
2021-06-06 17:13:26 +0300

    feat(reports): init report

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Report from './components/Report';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return (<Report></Report>
   );
 }
 
---
+++ components/Report.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Block} from 'react-native-block'
+export default function Report() {
+  return (
+    <View style={styles.container}>
+      <Text>דיווח</Text>
+      <StatusBar style="auto" />
+      <Block flex={2} center middle mt={10}>
+         // Your components
+      </Block>
+    </View>
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#367df7',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
