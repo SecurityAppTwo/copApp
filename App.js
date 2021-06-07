7b8647984dd8f31254fa793c9354f71d7c3f1ed9
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 14:55:18 +0300

    fix(app):restore app.js

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Identification from './components/identification.js'
 
 export default function App() {
   return (
-    // <View>
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <View style={{ height: '100%' ,width: '100%'}}>
-    <Identification></Identification>
+    <View>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
-    // </View>
+    </View>
     
   );
 }
