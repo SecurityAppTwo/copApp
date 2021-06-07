941891417b0294e576f53836b2557e4982d0064e
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 11:59:54 +0300

    feat(id): continue implement

--- components/identification.js
+++ components/identification.js
@@ -1,61 +1,73 @@
-import React from 'react';
-import { View, KeyboardAvoidingView, TextInput, StyleSheet,  Platform, TouchableWithoutFeedback, Keyboard, } from 'react-native';
-import { Modal, Portal,Text, Button, Provider } from 'react-native-paper';
+import React, { useEffect } from 'react';
+import { View, KeyboardAvoidingView, TextInput, StyleSheet, Platform, TouchableWithoutFeedback, Keyboard, ImageBackground, Alert } from 'react-native';
+import { Modal, Portal, Text, Button, Provider } from 'react-native-paper';
+
 const Identification = () => {
     let [text, onChangeText] = React.useState(null);
-    const [visible, setVisible] = React.useState(false);
-    const showModal = () => {setVisible(true)};
-    const hideModal = () => setVisible(false);
-    const containerStyle = { backgroundColor: 'white', padding: 20, width:'50%', height:'50%', display: 'flex', alignItems: 'center'};
-    let info='';
-    const check = () => {
-        if(!suspectsids[text] && notid[text]){
-            info=`${notid[text]}
-            האם חשוד: לא`;
-        } else if(suspectsids[text] && !notid[text]){
-            info=`${suspectsids[text]}
-            האם חשוד: כן`;
-        } else {
-            info=`הת.ז לא נמצא במאגר`;
-        }
-        alert(info)
-        showModal();
+    const [suspect, setSuspect] = React.useState(undefined);
 
+    // const [visible, setVisible] = React.useState(false);
+    // const showModal = () => { setVisible(true) };
+    // const hideModal = () => setVisible(false);
+    // const [suspect, setSuspect] = React.useState('');
+    useEffect((suspect) => {
+        Alert.alert(
+            `${suspect.name}`,
+            `האם חשוד: ${suspect.isSuspect ? 'כן' : 'לא'}`,
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                { text: "OK", onPress: () => console.log("OK Pressed") }
+            ]
+        );
+    }, [suspect]);
+
+
+    const containerStyle = { backgroundColor: 'white', padding: 20, width: '50%', height: '50%', display: 'flex', alignItems: 'center' };
+    const check = () => {
+        let suspect;
+        if (!suspectsids[text] && notid[text]) {
+            suspect = {name: notid[text], isSuspect: false};
+            setSuspect({name: notid[text], isSuspect: false});
+        } else if (suspectsids[text] && !notid[text]) {
+            suspect = {name: suspectsids[text], isSuspect: true}
+            setSuspect({name: suspectsids[text], isSuspect: true});
+        } else {
+            setInfo('הת.ז לא נמצא במאגר')
+        }
+        // showModal();
+        alert(text)
+        // alert(suspect)
+        // createTwoButtonAlert(suspect);
     }
-    const notid={
-        "000000000":"lili blabla"
+    const notid = {
+        "000000000": "lili blabla"
     }
-    const suspectsids ={
-        "123456789":"ג'ק המחסל"
+    const suspectsids = {
+        "123456789": "ג'ק המחסל"
     };
 
     return (
-        // <KeyboardAvoidingView
-        //   behavior={Platform.OS === "ios" ? "padding" : "height"}
-        //   style={styles.container}
-        // >
-        //   <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.inner}>
-            <Text style={styles.header}>זיהוי לפי ת.ז</Text>
-            <Text style={styles.id}>הכנס מס' תעודת זהות</Text>
-            <TextInput style={styles.textInput} onChangeText={(val) => onChangeText(val)}
-                value={text} />
-            <View style={styles.btnContainer}>
-            <Button title="קבל מידע רלוונטי" onPress={() => check()} >
-                    <Text style={{ color: "blue" }}>קבל מידע רלוונטי</Text>
-                </Button>
+        <ImageBackground source={require('../assets/id.jpg')} style={styles.image}>
+            <View style={styles.inner}>
+                <View style={{ backgroundColor: "rgba(255, 255, 255, 0.8)", marginTop: 100, borderRadius: 15 }}>
+                    <Text style={styles.header} >זיהוי לפי ת.ז</Text>
+                    <Text style={styles.id}>הכנס מס' תעודת זהות</Text>
+                    <TextInput style={styles.textInput} onChangeText={(val) => onChangeText(val)}
+                        value={text}  />
+                </View>
+                <View style={styles.btnContainer}>
+                    <Button title="קבל מידע רלוונטי" onPress={() => check()} >
+                        <Text style={{ color: "white", fontSize: 30 }}>קבל מידע רלוונטי</Text>
+                    </Button>
+
+                </View>
 
             </View>
-            <Provider >
-      <Portal>
-        <Modal style={{display: 'flex', alignItems: 'center'}} visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
-          <Text>{info}</Text>
-        </Modal>
-      </Portal>
-    </Provider>
-        </View>
-        //  {/* </TouchableWithoutFeedback>
-        // </KeyboardAvoidingView> */}
+        </ImageBackground>
     );
 };
 
@@ -67,19 +79,23 @@ const styles = StyleSheet.create({
         fontSize: 36,
         padding: 24,
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        alignContent:"center",
-        backgroundColor: `#c0c0c0`
+
+
     },
     header: {
         fontSize: 36,
-        marginBottom: 48
+        marginBottom: 48,
+        opacity: 1,
+        textAlign: "center",
+        color: "midnightblue",
+        marginTop: 20,
+
     },
     id: {
         marginTop: 30,
         fontSize: 30,
-        color: `#fffafa`
+        color: `midnightblue`,
+        textAlign: "center"
     },
     textInput: {
         marginTop: 30,
@@ -87,12 +103,26 @@ const styles = StyleSheet.create({
         borderColor: "#000000",
         borderWidth: 1,
         borderBottomWidth: 1,
+        marginBottom: 10,
+        borderRadius: 10,
+        textAlign: "center",
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: "midnightblue"
     },
     btnContainer: {
-        backgroundColor: "turquoise",
-        color: "blue",
-        marginTop: 50
-    }
+        color: "white",
+        marginTop: 150,
+        borderWidth: 1,
+        borderColor: "white",
+        borderRadius: 10
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+        width: '100%'
+    },
 });
 
 
